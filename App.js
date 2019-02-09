0d81f0772a511d84c54791f7c7391e53991c7e50
Oliver Gomes <ogomesphoto@gmail.com>
2019-02-09 00:24:00 -0500

    redux implement

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
 import React from "react";
 import { TouchableHighlight, StyleSheet, Text, View } from "react-native";
 import Counter from "./src/Counter";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const initialState = {
+  count: 0
+};
+
+const reducert = (state = initialState, action) => {
+  return state;
+};
+
+const store = createStore(reducer);
 
 export default class App extends React.Component {
   render() {
-    return <Counter />;
+    return (
+      <Provider store={store}>
+        <Counter />
+      </Provider>
+    );
   }
 }
