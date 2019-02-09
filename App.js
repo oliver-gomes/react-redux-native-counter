08034e71cdb7600d76bdbd6418b21a889cf2f1c7
Oliver Gomes <ogomesphoto@gmail.com>
2019-02-09 00:18:05 -0500

    Counter implement

--- App.js
+++ App.js
@@ -1,21 +1,9 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { TouchableHighlight, StyleSheet, Text, View } from "react-native";
+import Counter from "./src/Counter";
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    return <Counter />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
